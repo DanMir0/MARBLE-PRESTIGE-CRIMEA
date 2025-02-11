bceccc1dbcdd6ab11c2a1827ab06e83bfd2bb6ea
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2025-02-11 20:06:05 +0300

    adding vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import './assets/main.css'
-
-import { createApp } from 'vue'
+import {createApp} from "vue";
 import App from './App.vue'
+import 'vuetify/_styles.scss';
+import vuetify from "@/vuetify.js";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app
+  .use(vuetify)
+  .mount('#app')
---
+++ src/vuetify.js
@@ -0,0 +1,28 @@
+import {createVuetify} from 'vuetify';
+import * as components from 'vuetify/components'; // Импорт всех компонентов
+import * as directives from 'vuetify/directives'; // Импорт всех директив
+
+const vuetify = createVuetify({
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    themes: {
+      light: {
+        colors: {
+          primary: '#9155FD', // основной цвет
+          secondary: '#8A8D93', // второстепенный цвет
+          accent: '#0d6efd',
+          success: '#56CA00',
+          info: '#16B1FF',
+          warning: '#FFB400',
+          error: '#FF4C51',
+        },
+      },
+    },
+  },
+  components,
+  directives,
+})
+
+export default vuetify;
